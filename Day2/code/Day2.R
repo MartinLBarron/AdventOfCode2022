c7f31a61f86d6bf5ada747f15a6488ebe2f1521a
Martin Barron <barronm@uchicago.edu>
2022-12-02 12:40:25 -0600

    Dry pass

--- Day2/code/Day2.R
+++ Day2/code/Day2.R
@@ -2,6 +2,25 @@
 # https://adventofcode.com/2022/day/2
 library(tidyverse)
 
+score_game <-function(opponent, self){
+  return (case_when(opponent=="Rock" & self=="Rock" ~ 3,
+                    opponent=="Rock" & self=="Paper" ~ 6,
+                    opponent=="Rock" & self=="Scissors" ~ 0,
+                    opponent=="Paper" & self=="Rock" ~ 0,
+                    opponent=="Paper" & self=="Paper" ~ 3,
+                    opponent=="Paper" & self=="Scissors" ~ 6,
+                    opponent=="Scissors" & self=="Rock" ~ 6,
+                    opponent=="Scissors" & self=="Paper" ~ 0,
+                    opponent=="Scissors" & self=="Scissors" ~ 3))
+  
+}
+
+score_pick <-function(self){
+  return(case_when(self == "Rock" ~ 1,
+                   self == "Paper" ~ 2,
+                   self == "Scissors" ~3))
+}
+
 df <- read_delim("Day2/data/input.txt", col_names=F, delim=" ") %>%
   mutate(opponent=fct_recode(X1,"Rock" = "A",
                              "Paper" = "B",
@@ -9,18 +28,8 @@ df <- read_delim("Day2/data/input.txt", col_names=F, delim=" ") %>%
          self = fct_recode(X2, "Rock" = "X",
                            "Paper" = "Y",
                            "Scissors" = "Z"),
-         score_pick=case_when(self == "Rock" ~ 1,
-                              self == "Paper" ~ 2,
-                              self == "Scissors" ~3),
-         score_game=case_when(opponent=="Rock" & self=="Rock" ~ 3,
-                              opponent=="Rock" & self=="Paper" ~ 6,
-                              opponent=="Rock" & self=="Scissors" ~ 0,
-                              opponent=="Paper" & self=="Rock" ~ 0,
-                              opponent=="Paper" & self=="Paper" ~ 3,
-                              opponent=="Paper" & self=="Scissors" ~ 6,
-                              opponent=="Scissors" & self=="Rock" ~ 6,
-                              opponent=="Scissors" & self=="Paper" ~ 0,
-                              opponent=="Scissors" & self=="Scissors" ~ 3),
+         score_pick=score_pick(self),
+         score_game=score_game(opponent, self),
          score_total=score_pick+score_game)
 
 answer_part1 <- sum(df$score_total)
@@ -43,20 +52,9 @@ df2 <- df %>%
                      opponent=="Scissors" & outcome=="win" ~ "Rock",
                      opponent=="Scissors" & outcome=="draw" ~ "Scissors",
                      opponent=="Scissors" & outcome=="lose" ~ "Paper"),
-    score_pick=case_when(self == "Rock" ~ 1,
-                         self == "Paper" ~ 2,
-                         self == "Scissors" ~3),
-    score_game=case_when(opponent=="Rock" & self=="Rock" ~ 3,
-                         opponent=="Rock" & self=="Paper" ~ 6,
-                         opponent=="Rock" & self=="Scissors" ~ 0,
-                         opponent=="Paper" & self=="Rock" ~ 0,
-                         opponent=="Paper" & self=="Paper" ~ 3,
-                         opponent=="Paper" & self=="Scissors" ~ 6,
-                         opponent=="Scissors" & self=="Rock" ~ 6,
-                         opponent=="Scissors" & self=="Paper" ~ 0,
-                         opponent=="Scissors" & self=="Scissors" ~ 3),
+    score_pick=score_pick(self),
+    score_game=score_game(opponent, self),
     score_total=score_pick+score_game)
-
 
 answer_part2 <- sum(df2$score_total)
 
